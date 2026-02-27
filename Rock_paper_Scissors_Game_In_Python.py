b1abfca4da2815ff38ab09abfd5cdee48d06a9cd
Sahil <sahil514757@gmail.com>
2026-02-27 12:58:10 +0530

    Rock Paper Scissors Game 🎯
    You choose one and computer choose one and show how win, it is so funny and knowledging program.

---
+++ Rock_paper_Scissors_Game_In_Python.py
@@ -0,0 +1,37 @@
+import random
+
+choices = ["rock", "paper", "scissors"]
+user_score = 0
+computer_score = 0
+
+while True:
+    user = input("\nChoose rock / paper / scissors (or quit): ").lower()
+    
+    if user == "quit":
+        break
+
+    if user not in choices:
+        print("Invalid choice!")
+        continue
+
+    computer = random.choice(choices)
+    print("Computer chose:", computer)
+
+    if user == computer:
+        print("Draw")
+
+    elif (user == "rock" and computer == "scissors") or \
+         (user == "paper" and computer == "rock") or \
+         (user == "scissors" and computer == "paper"):
+        print("You Win 🎉")
+        user_score += 1
+
+    else:
+        print("You Lose ❌")
+        computer_score += 1
+
+    print(f"Score → You: {user_score} | Computer: {computer_score}")
+
+print("\nFinal Score")
+print(f"You: {user_score} | Computer: {computer_score}")
+print("Thanks for playing 😊")
